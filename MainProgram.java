637f6c6f3c462bad9ffae94865d11bb96fc48d6c
williamisfranciscodasilva <williamisepaula@gmail.com>
2019-01-18 16:30:34 -0200

    Programa em Java que soma

---
+++ MainProgram.java
@@ -0,0 +1,13 @@
+package procedimento;
+
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		
+		processo p = new processo();
+		p.soma(5,2);
+		
+	}
+
+}
---
+++ processo.java
@@ -0,0 +1,15 @@
+package procedimento;
+
+import javax.swing.JOptionPane;
+
+public class processo {
+	
+	public static void soma(int a, int b) {
+		
+		int s = a + b;
+		JOptionPane.showMessageDialog(null, "Mostra a soma de "+a +" + "+b+ " é "+s);
+		System.out.println("Mostra a soma de "+a +" + "+b+ " é "+s);
+		
+	    }
+	
+   }
